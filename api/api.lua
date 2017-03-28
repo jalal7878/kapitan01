8ddd7f4f1ae9cb32b7ab6b3acd90180e8b9ae582
jalal7878 <jalal7878@users.noreply.github.com>
2017-03-28 14:14:38 +0430

    Update api.lua

--- api/api.lua
+++ api/api.lua
@@ -7,9 +7,9 @@ local url = 'https://api.telegram.org/bot' .. token
 local offset = 0
 local redis = require('redis')
 local redis = redis.connect('127.0.0.1', 6379)
-local SUDO = 304107094
+local SUDO = 262853461
 function is_mod(chat,user)
-sudo = {304107094}
+sudo = {262853461}
   local var = false
   for v,_user in pairs(sudo) do
     if _user == user then
